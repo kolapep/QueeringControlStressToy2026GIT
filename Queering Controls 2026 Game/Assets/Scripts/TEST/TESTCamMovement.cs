28d4b2a7943a89b6d55b80d2c2c5930ceba0a547
vega jafri <giantwhitedeer@gmail.com>
2026-03-28 06:22:33 -0400

    level design polish; camera movement fix

--- Queering Controls 2026 Game/Assets/Scripts/TEST/TESTCamMovement.cs
+++ Queering Controls 2026 Game/Assets/Scripts/TEST/TESTCamMovement.cs
@@ -4,25 +4,56 @@ using System.Collections.Generic;
 
 public class TESTCamMovement : MonoBehaviour
 {
-    public float speed = 1f;
-    private float endPos = 50f;
+//     public float speed = 1f;
+//     private float endPos = 50f;
+//     public GameObject replayButton;
+
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         Camera.main.transform.position = new Vector3(speed + transform.position.x, transform.position.y, transform.position.z);
+
+//         if (Camera.main.transform.position.x >= endPos)
+//         {
+//             speed = 0;
+//             replayButton.SetActive(true);
+//         }
+//     }
+// }
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 5f;
     public GameObject replayButton;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool reachedEnd = false;
+
     void Start()
     {
-        
+        transform.position = pointA.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Camera.main.transform.position = new Vector3(speed + transform.position.x, transform.position.y, transform.position.z);
+        if (reachedEnd) return;
 
-        if (Camera.main.transform.position.x >= endPos)
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            pointB.position,
+            speed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, pointB.position) < 0.01f)
         {
-            speed = 0;
-            replayButton.SetActive(true);
+            reachedEnd = true;
+
+            if (replayButton != null)
+                replayButton.SetActive(true);
         }
     }
 }
